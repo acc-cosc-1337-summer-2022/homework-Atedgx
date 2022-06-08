c52816ec84ceeef5c6cdbebd6ae075c270379e28
Atedgx <106637734+Atedgx@users.noreply.github.com>
2022-06-07 23:00:36 -0500

    Homework

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,9 +1,15 @@
 #include "expressions.h"
 
 //example
-int add_numbers(int num1, int num2)
+double get_sales_tax_amount(double meal_amount);
 {
-	return num1 + num2;
+	product = tax_rate * meal_amount;
+	return product;
+}
+double get_tip_amount(double meal_amount, double tip_rate);
+{
+	tip_rate_product = meal_amount * tip_rate;
+	return tip_rate_product;
 }
 
 //write function code here
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,35 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount = 0;
+	double tip_rate = 0;
+	double tip_amount = 0;
+	double tax_amount = 0;
+	double total = 0;
+	cout<<"How much was the total of your meal?";
+	cin>>meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+	cout<<"How much do you want to tip";
+	cin>>tip_rate;
+	tip_amount = (get_tip_amount(meal_amount,tip_rate));
+	full_meal_cost = (tip_amount + tax_amount + meal_amount);
+	cout<<"The final cost of the meal with tip: ";
+	cout<<"Meal Amount:"<<"\t"<<meal_amount<<"\n";
+	cout<<"Sales Tax:"<<"\t"<<tax_amount<<"\n";
+	cout<<"Tip Amount:"<<"\t"<<tip_amount<<"\n";
+	cout<<"Total:"<<"\t"<<full_meal_cost<<"\n";
+
+
 
 	return 0;
 }
