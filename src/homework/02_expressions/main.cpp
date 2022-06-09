90a3c3d4c4fa8f165782d4910ebab146a652ae2c
Atedgx <zachary.powell@g.austincc.edu>
2022-06-08 21:26:58 -0500

    edited the std:: command to include cout and cin at top of page rather than inline

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include "hwexpressions.h"
+using std::cout;
+using std::cin;
 
 int main()
 {
@@ -8,20 +10,20 @@ int main()
     double tip_amount = 0;
     double tax_amount = 0;
     double total = 0;
-    std::cout<<"How much was the total of your meal?: ";
-    std::cin>>meal_amount;
+    cout<<"How much was the total of your meal?: ";
+    cin>>meal_amount;
     tax_amount = get_sales_tax_amount(meal_amount);
-    std::cout<<"How much would you like to tip?: ";
-    std::cin>>tip_rate;
+    cout<<"How much would you like to tip?: ";
+    cin>>tip_rate;
     tip_amount = (get_tip_amount(meal_amount,tip_rate));
 	tip_amount = tip_amount/400;
     total = (tip_amount + tax_amount + meal_amount);
 	tip_amount = tip_amount;
-    std::cout<<"The final cost of the meal with tip: ";
-    std::cout<<"Meal Amount:"<<"\t"<<meal_amount<<"\n";
-    std::cout<<"Sales Tax:"<<"\t"<<tax_amount<<"\n";
-    std::cout<<"Tip percent was: "<<"\t"<<tip_amount<<"\n";
-    std::cout<<"Total:"<<"\t"<<total<<"\n";
+    cout<<"The final cost of the meal with tip: ";
+    cout<<"Meal Amount:"<<"\t"<<meal_amount<<"\n";
+    cout<<"Sales Tax:"<<"\t"<<tax_amount<<"\n";
+    cout<<"Tip percent was: "<<"\t"<<tip_amount<<"\n";
+    cout<<"Total:"<<"\t"<<total<<"\n";
 
 
 
